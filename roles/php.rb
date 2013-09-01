08c85b125d28d2c035c62ffddb0d5ab3e5af3bb2
Chris Blackburn <christopher.blackburn@gmail.com>
2013-09-01 12:01:22 +0100

    Added CURL to PHP

--- roles/php.rb
+++ roles/php.rb
@@ -3,10 +3,10 @@ description "PHP scripting language"
 run_list(
   "recipe[php]",
   "recipe[apache2::mod_php5]",
-  "recipe[phpunit]"
+  "recipe[php::module_curl]"
 )
 override_attributes(
-  "php" => { 
-    "version" => "5.3.10" 
+  "php" => {
+    "version" => "5.3.10"
   }
 )
