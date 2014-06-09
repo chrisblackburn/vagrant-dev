6d67c589d9f7ae451949ed1886498707145f7f99
Luke Morton <luke@madebymade.co.uk>
2014-06-09 14:23:13 +0100

    Add better nodejs cookbook

--- roles/development.rb
+++ roles/development.rb
@@ -4,11 +4,15 @@ run_list(
   "recipe[php]",
   "recipe[apache2::mod_php5]",
   "recipe[phpmyadmin]",
-  "recipe[cf]"
+  "recipe[cf]",
+  "recipe[nodejs]"
 )
 override_attributes(
   "phpmyadmin" => { 
     "host" => "127.0.0.1",
     "hostname" => "phpmyadmin.local" 
+  },
+  "nodejs" => {
+    "version" => "0.10.28"
   }
 )
