fe4c193b9ad20b4ec0d01e218c0c69bf43e53585
David Winter <i@djw.me>
2015-03-17 15:48:00 +0000

    Apply phantomjs config recipe in tests role.

--- roles/test.rb
+++ roles/test.rb
@@ -2,6 +2,7 @@ name "test"
 description "Role that can be used for testing webapps"
 run_list(
   "recipe[phantomjs]",
+  "recipe[phantomjs::config]",
   "recipe[jscoverage]"
 )
 override_attributes(
